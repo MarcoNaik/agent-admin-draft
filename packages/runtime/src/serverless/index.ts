79c098a11e07fe96ce3ce08418a1d77a5ec95747
marcoNaik <marcogomezf@gmail.com>
2026-01-30 17:13:16 +1100

    refactor(packages): remove runtime package in favor of unified struere package

--- packages/runtime/src/serverless/index.ts
+++
@@ -1,3 +0,0 @@
-export { createHandler, type ServerlessRequest, type ServerlessResponse, type HandlerOptions } from './handler.js'
-export { createVercelHandler, type VercelRequest, type VercelContext, config as vercelConfig } from './vercel.js'
-export { createLambdaHandler, createLambdaStreamingHandler, type LambdaEvent, type LambdaContext, type LambdaResponse } from './lambda.js'
--- packages/runtime/src/state/index.ts
+++
@@ -1,3 +0,0 @@
-export { MemoryStateBackend } from './MemoryStateBackend.js'
-export { ConversationStore } from './ConversationStore.js'
-export type { Conversation } from './ConversationStore.js'
