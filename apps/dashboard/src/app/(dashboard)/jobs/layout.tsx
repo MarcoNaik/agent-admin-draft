baa8f93e378b4fb2b3a6556aabe5e1326f973076
marcoNaik <marcogomezf@gmail.com>
2026-02-18 14:34:13 +1100

    chore(apps/dashboard): delete jobs route pages and jobs-dashboard-realtime component

--- apps/dashboard/src/app/(dashboard)/jobs/layout.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { AdminOnly } from "@/components/role-redirect"
-
-export default function JobsLayout({ children }: { children: React.ReactNode }) {
-  return <AdminOnly>{children}</AdminOnly>
-}
