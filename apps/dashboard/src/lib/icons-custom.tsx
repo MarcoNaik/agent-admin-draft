b19f4e19ecd73b08245965a10b6f4c3afa08ef72
marcoNaik <marcogomezf@gmail.com>
2026-03-10 15:23:59 +1100

    feat(dashboard/lib): add icons barrel re-exporting pixelarticons with aliases

---
+++ apps/dashboard/src/lib/icons-custom.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+export const Sun = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M11 1h2v4h-2V1zM3.515 3.515l1.414 1.414L3.1 6.757 1.686 5.343l1.829-1.828zM19.071 4.929l1.414-1.414 1.829 1.828-1.414 1.414-1.829-1.828zM11 7h2v2h2v2h2v2h-2v2h-2v2h-2v-2H9v-2H7v-2h2V9h2V7zM1 11h4v2H1v-2zm18 0h4v2h-4v-2zM4.929 19.071l1.414-1.414 1.828 1.829-1.414 1.414-1.828-1.829zm14.142 0l-1.414-1.414-1.828 1.829 1.414 1.414 1.828-1.829zM11 19h2v4h-2v-4z" />
+  </svg>
+)
+
+export const PanelLeft = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M2 3h20v2H2V3zM2 19h20v2H2v-2zM2 5h2v14H2V5zm18 0h2v14h-2V5zM8 5h2v14H8V5z" />
+  </svg>
+)
+
+export const PanelRight = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M2 3h20v2H2V3zM2 19h20v2H2v-2zM2 5h2v14H2V5zm18 0h2v14h-2V5zM14 5h2v14h-2V5z" />
+  </svg>
+)
+
+export const X = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M5 5h2v1H5zM17 5h2v1h-2zM6 6h2v1H6zM16 6h2v1h-2zM7 7h2v1H7zM15 7h2v1h-2zM8 8h2v1H8zM14 8h2v1h-2zM9 9h2v1H9zM13 9h2v1h-2zM10 10h4v1h-4zM11 11h2v1h-2zM10 12h4v1h-4zM9 13h2v1H9zM13 13h2v1h-2zM8 14h2v1H8zM14 14h2v1h-2zM7 15h2v1H7zM15 15h2v1h-2zM6 16h2v1H6zM16 16h2v1h-2zM5 17h2v1H5zM17 17h2v1h-2z" />
+  </svg>
+)
+
+export const Key = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M13 3h4v2h2v2h2v4h-2v2h-2v2h-4v-2h-2v2H9v2H7v2H5v2H3v2H1v-4h2v-2h2v-2h2v-2h2v-2h2V9h2V3zm2 2v2h2v2h2V7h-2V5h-2zm0 4h-2v2h2V9z" />
+  </svg>
+)
---
+++ apps/dashboard/src/lib/icons.ts
@@ -0,0 +1,117 @@
+export type { SVGProps } from "react"
+
+export type IconComponent = React.ComponentType<React.SVGProps<SVGSVGElement>>
+
+export {
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  Bell,
+  Blocks,
+  Calendar,
+  Check,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  Circle,
+  Clock,
+  Copy,
+  Cpu,
+  CreditCard,
+  Database,
+  ExternalLink,
+  Eye,
+  EyeOff,
+  FileText,
+  Globe,
+  Link,
+  Lock,
+  Mail,
+  Mic,
+  Minus,
+  Monitor,
+  Moon,
+  MoreHorizontal,
+  Phone,
+  Play,
+  Plus,
+  Power,
+  PowerOff,
+  Search,
+  Send,
+  Server,
+  Shield,
+  Smartphone,
+  Sparkles,
+  Square,
+  Terminal,
+  Unlink,
+  User,
+  UserPlus,
+  Users,
+  Wifi,
+  Zap,
+} from "pixelarticons/react"
+
+export { Analytics as Activity } from "pixelarticons/react"
+export { SquareAlert as AlertCircle } from "pixelarticons/react"
+export { WarningDiamond as AlertTriangle } from "pixelarticons/react"
+export { ArrowsHorizontal as ArrowRightLeft } from "pixelarticons/react"
+export { SortVertical as ArrowUpDown } from "pixelarticons/react"
+export { Cancel as Ban } from "pixelarticons/react"
+export { RobotFace as Bot } from "pixelarticons/react"
+export { AiScan as Brain } from "pixelarticons/react"
+export { CheckDouble as CheckCheck } from "pixelarticons/react"
+export { CheckboxOn as CheckCircle } from "pixelarticons/react"
+export { CheckboxOn as CheckCircle2 } from "pixelarticons/react"
+export { Clipboard as ClipboardCopy } from "pixelarticons/react"
+export { Braces as Code } from "pixelarticons/react"
+export { Coins as DollarSign } from "pixelarticons/react"
+export { PenSquare as Edit } from "pixelarticons/react"
+export { File as FileCode } from "pixelarticons/react"
+export { File as FileEdit } from "pixelarticons/react"
+export { SortVertical as Filter } from "pixelarticons/react"
+export { TestTube as FlaskConical } from "pixelarticons/react"
+export { InfoBox as HelpCircle } from "pixelarticons/react"
+export { Image as ImageIcon } from "pixelarticons/react"
+export { Flatten as Layers } from "pixelarticons/react"
+export { Link as Link2 } from "pixelarticons/react"
+export { Loader as Loader2 } from "pixelarticons/react"
+export { Message as MessageSquare } from "pixelarticons/react"
+export { PanelLeft, PanelRight } from "./icons-custom"
+export { Attachment as Paperclip } from "pixelarticons/react"
+export { PenSquare as Pencil } from "pixelarticons/react"
+export { Modem as Plug } from "pixelarticons/react"
+export { Reload as RefreshCw } from "pixelarticons/react"
+export { Download as Save } from "pixelarticons/react"
+export { SettingsCog as Settings } from "pixelarticons/react"
+export { Switch as ToggleLeft } from "pixelarticons/react"
+export { Delete as Trash2 } from "pixelarticons/react"
+export { RssCircle as Webhook } from "pixelarticons/react"
+export { Signal as WifiOff } from "pixelarticons/react"
+export { X } from "./icons-custom"
+export { CopyX as XCircle } from "pixelarticons/react"
+export { MoreVertical } from "pixelarticons/react"
+export { Skull } from "pixelarticons/react"
+export { Repeat } from "pixelarticons/react"
+export { BookOpen } from "pixelarticons/react"
+export { Reload as RotateCcw } from "pixelarticons/react"
+export { ChartBarBig as TrendingUp } from "pixelarticons/react"
+export { Cancel as XOctagon } from "pixelarticons/react"
+export { AlarmClock as Timer } from "pixelarticons/react"
+export { BuildingCommunity as Building2 } from "pixelarticons/react"
+export { ChartBarBig as BarChart3 } from "pixelarticons/react"
+export { Shield as ShieldCheck } from "pixelarticons/react"
+export { Shield as ShieldX } from "pixelarticons/react"
+export { ChevronsVertical as ChevronsUpDown } from "pixelarticons/react"
+export { Grid3x3 as LayoutGrid } from "pixelarticons/react"
+export { ListBox as List } from "pixelarticons/react"
+export { ListBox as LayoutList } from "pixelarticons/react"
+export { Circle as CircleDot } from "pixelarticons/react"
+
+export { Sun, Key } from "./icons-custom"
+export { ToolCase as Wrench } from "pixelarticons/react"
+export { Message as MessageCircle } from "pixelarticons/react"
+export { Grid2x3 as Table } from "pixelarticons/react"
